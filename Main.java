7afb5826a3b24ec76cf6f9e6b46fb2f13a2ce153
lxieww <melovitoria763@gmail.com>
2025-10-07 19:21:42 -0300

    Atualizando o Main para simular a ordem complexa de bolos

--- Main.java
+++ Main.java
@@ -5,8 +5,29 @@ public class Main {
     public static void main(String[] args) {
         // Create the order
         Order order = new Order();
-        order.addCake(new VanillaCake());
+        
+        // 1. Bolo de chocolate [cite: 213]
         order.addCake(new ChocolateCake());
+
+        // 2. Bolo de baunilha com o dizer "PLAIN!" [cite: 214]
+        Cake cake2 = new SayingCake(new VanillaCake(), "PLAIN!");
+        order.addCake(cake2);
+
+        // 3. Bolo de baunilha com granulado com os dizeres "FANCY" [cite: 215]
+        Cake cake3 = new SayingCake(new SprinklesCake(new VanillaCake()), "FANCY");
+        order.addCake(cake3);
+        
+        // 4. Bolo de morango em várias camadas com granulado duplo e dois dizeres
+        // "One of" e "EVERYTHING" [cite: 217]
+        Cake cake4 = new SayingCake(
+                        new SayingCake(
+                            new SprinklesCake(
+                                new SprinklesCake(
+                                    new MultiLayeredCake(
+                                        new StrawberryCake()))),
+                            "EVERYTHING"), 
+                        "One of");
+        order.addCake(cake4);
 
         // Print the order
         order.printOrder();
